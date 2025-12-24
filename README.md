f733ff16b865487a3d347f01671ae03f1f861657
tungtq <kibo0603@gmail.com>
2025-12-24 15:40:17 +0700

    update readmi

---
+++ README.md
@@ -0,0 +1,5 @@
+# OCR Document System
+
++ Programming Language: Python
++ Framework: Pyramid
++ Download Package: [Tesseract](https://github.com/UB-Mannheim/tesseract/wiki)
--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import os
 from PIL import Image
 import pytesseract
 
-import pytesseract
-
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 
